98a13e40a5bfc18038db87da793af8191ac4e9ad
GerMP21 <german.martinez.pascale@gmail.com>
2023-10-03 10:46:22 -0300

    Added world class

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const font = new FontFace("VT323", "url(assets/VT323-Regular.ttf)", {
+  style: "regular",
+  weight: "400",
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
