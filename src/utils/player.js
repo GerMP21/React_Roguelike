2bc54b2e6f1e8802d90bdf981009e73193f254d7
GerMP21 <german.martinez.pascale@gmail.com>
2023-10-03 09:18:06 -0300

    Added Entity and Player classes

---
+++ src/utils/player.js
@@ -0,0 +1,31 @@
+import Entity from './Entity.js';
+
+class Player extends Entity {
+  inventory = [];
+
+  attributes = {
+    name: 'Player',
+    color: 'white'
+    ascii: '@',
+    health: 10
+  };
+
+  move(dx, dy) {
+    if (this.attributes.health <= 0) return;
+    this.x += dx;
+    this.y += dy;
+  }
+
+  copy() {
+    let newPlayer = new Player();
+    Object.assign(newPlayer, this);
+    return newPlayer;
+  }
+
+  addToInventory(item) {
+    this.inventory.push(item);
+  }
+
+}
+
+export default Player;
